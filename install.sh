3665585a7c9b34c273345bc5ac1f7d63c15189e7
Dz <99237626+Dafatrizaaa@users.noreply.github.com>
2025-01-18 12:41:04 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -92,35 +92,35 @@ bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoAddons
 }
 
 More_Buttons() {
-bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoAddons/addons/version1.x/More_Buttons/build.sh)
+bash <(curl -s https://raw.githubusercontent.com/Dafatrizaaa/Pterodactyl-AutoAddons/addons/version1.x/More_Buttons/build.sh)
 }
 
 More_Server_Info() {
-bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoAddons/addons/version1.x/More_Server_Info/build.sh)
+bash <(curl -s https://raw.githubusercontent.com/Dafatrizaaa/Pterodactyl-AutoAddons/addons/version1.x/More_Server_Info/build.sh)
 }
 
 Server_Router_Icons() {
-bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoAddons/addons/version1.x/Server_Router_Icons/build.sh)
+bash <(curl -s https://raw.githubusercontent.com/Dafatrizaaa/Pterodactyl-AutoAddons/addons/version1.x/Server_Router_Icons/build.sh)
 }
 
 PMA_Button_NavBar() {
-bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoAddons/addons/version1.x/PMA_Button_NavBar/build.sh)
+bash <(curl -s https://raw.githubusercontent.com/Dafatrizaaa/Pterodactyl-AutoAddons/addons/version1.x/PMA_Button_NavBar/build.sh)
 }
 
 PMA_Button_Database_Tab() {
-bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoAddons/addons/version1.x/PMA_Button_Database_Tab/build.sh)
+bash <(curl -s https://raw.githubusercontent.com/Dafatrizaaa/Pterodactyl-AutoAddons/addons/version1.x/PMA_Button_Database_Tab/build.sh)
 }
 
 MC_Paste() {
-bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoAddons/addons/version1.x/MC_Paste/build.sh)
+bash <(curl -s https://raw.githubusercontent.com/Dafatrizaaa/Pterodactyl-AutoAddons/addons/version1.x/MC_Paste/build.sh)
 }
 
 Bigger_Console() {
-bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoAddons/addons/version1.x/Bigger_Console/build.sh)
+bash <(curl -s https://raw.githubusercontent.com/Dafatrizaaa/Pterodactyl-AutoAddons/addons/version1.x/Bigger_Console/build.sh)
 }
 
 Files_In_Editor() {
-bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoAddons/addons/version1.x/Files_In_Editor/build.sh)
+bash <(curl -s https://raw.githubusercontent.com/Dafatrizaaa/Pterodactyl-AutoAddons/addons/version1.x/Files_In_Editor/build.sh)
 }
 
 
